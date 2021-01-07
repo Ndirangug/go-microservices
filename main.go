d911a457934d981262c9574184f863a2dc7c1f91
George Ndirangu <ndirangu.mepawa@gmail.com>
2021-01-08 00:21:15 +0300

    basic http server

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world /")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "OOps!", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
+
+	})
+
+	http.HandleFunc("/george", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello george")
+	})
+
+	http.ListenAndServe("localhost:9090", nil)
 }
